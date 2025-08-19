e2d955631bd24a7261ad331a9f9f328235dc9e26
sergioxv2006 <paulosergiopaulinho449@gmail.com>
2025-08-19 16:05:45 -0300

    novo código python

--- custo_viagem.py
+++ custo_viagem.py
@@ -1,11 +1,15 @@
 # Planejador de Custo de Viagem
 print("--- Planejador de Custo de Viagem ---")
 
-distancia = float(input("Digite a distância da viagem em km:"))
-consumo = float(input("Digite o consumo do veículo em km/litro:"))
-preco_combustivel = float(input("Digite o preço do combustível por litro: R$"))
+try:
+    distancia = int(input("Digite a distância da viagem em km: "))
+    preco_gasolina = float(input("Digite o preço do litro da gasolina: "))
+    consumo = float(input("Digite o consumo do veículo em km/litro: "))
 
-custo_viagem = (distancia / consumo) * preco_combustivel
-
-print("\n--- Resultado ---")
-print(f"O custo total da viagem é: R$ {custo_viagem:.2f}")
+    if distancia <= 0 or preco_gasolina <= 0 or consumo <= 0:
+        raise ValueError("Os valores devem ser positivos.")
+    else:
+        custo_viagem = (distancia / consumo) * preco_gasolina
+    print(f"O custo total da viagem é: R$ {custo_viagem:.2f}")
+except ValueError as e:
+    print(f"Erro: {e}. Por favor, insira valores válidos.")
